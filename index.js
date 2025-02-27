9e6eea4c4e024a6e89a3d32532b8d9e91d35aec8
AleksandraTronb <aleksandratronb@gmail.com>
2025-02-27 11:40:58 +0000

    Added the AI API integration

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
-function poemGenerator(event) {
-  event.preventDefault();
-
+function displayPoem(responce) {
+  console.log("poem generated");
   new Typewriter("#poem", {
-    strings:
-      "Як умру, то поховайте <br> Мене на могилі <br>Серед степу широкого <br>На Вкраїні милій,",
+    strings: responce.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
   });
+}
+
+function poemGenerator(event) {
+  event.preventDefault();
+  console.log("generate a poem");
+  let apiKey = "869fb0d3774e410b801b3o2atc64d943";
+  let userInstruction = document.querySelector("#user-instructions");
+  console.log(userInstruction.value);
+  let prompt = `Generate an Ukrainian poem about ${userInstruction.value}`;
+  let context = `User instructions:You are a romantic poem expert and love to write short poems. Your mission is generate 4 lines poem in Ukrainian language and separete each line with <br />. Make sure to follow user instruction.`;
+  let url = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+  axios.get(url).then(displayPoem);
 }
 let poemElement = document.querySelector("#poem-form");
 poemElement.addEventListener("submit", poemGenerator);
